98be379f53a13ea6889fe512c94b6fd7ca8e161b
Anand Damodaran <dvlpr.anand@gmail.com>
2019-01-07 20:30:41 +0530

    Firebase notification build update

---
+++ app/src/main/java/com/viginfotech/spark/firebase/FCMNotification.kt
@@ -0,0 +1,10 @@
+package com.viginfotech.spark.firebase
+
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class FCMNotification: FirebaseMessagingService() {
+    override fun onMessageReceived(remoteMessage: RemoteMessage?) {
+        super.onMessageReceived(remoteMessage)
+    }
+}
